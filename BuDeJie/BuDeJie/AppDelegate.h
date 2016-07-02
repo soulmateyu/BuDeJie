39d641bbaabfa7523a3d15c82b5244cc27f64a78
manager <soulmate@yuchaodemacbook-air.local>
2016-07-02 20:29:27 +0800

    创建了文件夹分类

---
+++ BuDeJie/BuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuDeJie
+//
+//  Created by 余朝 on 16/7/2.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
